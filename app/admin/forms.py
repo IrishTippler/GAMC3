1b0ebe524bf02f382f6b0797281a7cc7d1c85cd6
Viacheslav Ivanov <vivanov@nvg.ru>
2018-11-16 11:37:53 +0300

    Initial commit

---
+++ app/admin/forms.py
@@ -0,0 +1,45 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.ext.sqlalchemy.fields import QuerySelectField
+from wtforms.validators import DataRequired
+
+from ..models import Department, Role, Breed
+
+class BreedForm(FlaskForm):
+    """
+    Form for admin to add or edit a breed
+    """
+    name = StringField('Name', validators=[DataRequired()])
+    thcvalue = StringField('Thcvalue', validators=[DataRequired()])
+    outheight = StringField('outheight')
+    inheight = StringField('intheight')
+    submit = SubmitField('Submit')
+
+class DepartmentForm(FlaskForm):
+    """
+    Form for admin to add or edit a department
+    """
+    name = StringField('Name', validators=[DataRequired()])
+    description = StringField('Description', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+
+class RoleForm(FlaskForm):
+    """
+    Form for admin to add or edit a role
+    """
+    name = StringField('Name', validators=[DataRequired()])
+    description = StringField('Description', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+
+class EmployeeAssignForm(FlaskForm):
+    """
+    Form for admin to assign departments and roles to employees
+    """
+    department = QuerySelectField(query_factory=lambda: Department.query.all(),
+                                  get_label="name")
+    role = QuerySelectField(query_factory=lambda: Role.query.all(),
+                            get_label="name")
+    submit = SubmitField('Submit')
+
